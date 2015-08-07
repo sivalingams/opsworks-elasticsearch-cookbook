50dfc2984350f9bd140496c1822ae1ceb8cd0557
Sivalingam <sivalingam@freshdesk.com>
2015-08-08 03:45:36 +0530

    Changes

---
+++ layer-custom/metadata.rb
@@ -0,0 +1,7 @@
+name                'layer-custom'
+maintainer          ''
+maintainer_email    'ssivalngm+git [at] gmaxx [dot] com'
+license             'All rights reserved'
+description         'Deploying Elasticsearch with OpsWorks'
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version             '0.1.0'
