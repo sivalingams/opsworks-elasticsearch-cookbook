831d818bf49afe369900804842cee540795ad9f4
Sivalingam <sivalingam@freshdesk.com>
2015-08-12 03:28:44 +0530

    layer name changed for autodetection

--- elastic_search/recipes/node-autodetection.rb
+++ elastic_search/recipes/node-autodetection.rb
@@ -5,8 +5,8 @@ node[:deploy].each do |application, deploy|
   next unless deploy[:application_type] == 'rails' && node[:opsworks][:instance][:layers].include?('rails-app')
 
   elasticsearch_hosts = []
-  if node[:opsworks][:layers][:elasticsearch].present?
-    node[:opsworks][:layers][:elasticsearch][:instances].each do |instance_name, instance_config|
+  if node[:opsworks][:layers][:search].present?
+    node[:opsworks][:layers][:search][:instances].each do |instance_name, instance_config|
       elasticsearch_hosts << "#{instance_config[:private_dns_name]}:9200"
     end
   end
